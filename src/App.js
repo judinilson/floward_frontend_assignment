13ca3718d633af19dc11daec55aed813c1c4786f
judinilson monchacha <judinilson0000@gmail.com>
2022-10-26 18:56:45 +0400

    create auth route

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Routes,
+  Route,
+  Navigate,
+  useNavigate,
+  useLocation,
+} from "react-router-dom";
+
+import { history } from "./helpers";
+import { Nav } from "./components";
+import { PrivateRoute } from "./routes";
+import { Home } from "./pages/Home";
+import { Details } from "./pages/Details";
+import { Login } from "./pages/Auth";
+
+export { App };
 
 function App() {
+  // init custom history object to allow navigation from
+  // anywhere in the react app (inside or outside components)
+  history.navigate = useNavigate();
+  history.location = useLocation();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container bg-light">
+      <Nav />
+      <div className="container pt-4 pb-4">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <PrivateRoute>
+                <Home />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/details/:alphaCode"
+            element={
+              <PrivateRoute>
+                <Details />
+              </PrivateRoute>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </div>
     </div>
   );
 }
-
-export default App;
